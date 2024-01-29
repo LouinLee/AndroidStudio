e47cad4d5eecebba18b7380433f01e0ad896fbcd
Louin <134504671+LouinLee@users.noreply.github.com>
2024-01-29 21:53:25 +0800

    Latihan

---
+++ Application2/app/src/main/java/com/example/application2/ButtonActivity.java
@@ -0,0 +1,27 @@
+package com.example.application2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    Button supabutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+
+        supabutton = (Button) findViewById(R.id.supabutton);
+        supabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Buttons", "Button supa clicked");
+            }
+        });
+    }
+}
--- Application2/app/src/main/java/com/example/application2/MainActivity.java
+++ Application2/app/src/main/java/com/example/application2/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.application2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     TextView major;
     TextView hobby;
     Button button;
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,17 @@ public class MainActivity extends AppCompatActivity {
         major.setText("Information Systems");
         hobby.setText("Music");
         button.setText("Okay");
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
     }
 
     @Override
